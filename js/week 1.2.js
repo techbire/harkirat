40b11cfb2e98b45b5bb45af754937d9218415671
Ansh Gupta <techbire69@gmail.com>
2024-06-12 21:46:32 +0530

    Add files via upload

---
+++ js/week 1.2.js
@@ -0,0 +1,49 @@
+//counting till 99
+// let a=99;
+// for(i=1;i<a;i++){
+//     console.log(i);
+// }
+ 
+
+
+
+//counts down from 30 to 0
+// let a=30;
+// function countdown(){
+//     console.log(a);
+//     a--;
+//         if(a>=0){ 
+//             setTimeout(countdown, 1000);
+//     }
+// }
+// countdown();
+
+
+
+
+//calculate the time it takes between a setTimeout call and the inner function actually running
+// const startTime = Date.now();
+// setTimeout(() => {
+//     const endTime = Date.now();
+//     const elapsedTime = endTime - startTime;
+//     console.log(`Time elapsed: ${elapsedTime} milliseconds`);
+// }, 1000);
+
+
+
+
+//terminal clock
+// function displayClock() {
+//     const now = new Date();
+//     const hours = String(now.getHours()).padStart(2, '0');
+//     const minutes = String(now.getMinutes()).padStart(2, '0');
+//     const seconds = String(now.getSeconds()).padStart(2, '0');
+//     console.clear();
+//     console.log(`${hours}:${minutes}:${seconds}`);
+// }
+
+// setInterval(displayClock, 1000);
+
+
+
+
